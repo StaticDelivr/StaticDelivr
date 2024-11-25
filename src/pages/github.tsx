6be823a7aa5bc76d449d428f46b8c3aca0427d55
Coozywana <73876112+coozywana@users.noreply.github.com>
2024-11-25 21:10:19 +1100

    Fix: Type error: Parameter 'e' implicitly has an 'any' type.

--- src/pages/github.tsx
+++ src/pages/github.tsx
@@ -37,7 +37,7 @@ const GitHubConverterPage = () => {
     setError('');
   };  
 
-  const handleSubmit = (e) => {
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
     convertUrl(githubUrl.trim());
   };
