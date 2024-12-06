68f1518c22a170e69f65b01488b076a76602a33c
Coozywana <73876112+coozywana@users.noreply.github.com>
2024-12-06 16:56:21 +1100

    Move icons into seperate folder

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,10 +7,10 @@ function MyApp({ Component, pageProps }) {
   return (
     <ParallaxProvider>
       <Head>
-        <link rel="icon" type="image/svg+xml" href="/assets/img/favicon.svg" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/favicon-16x16.png" />
-        <link rel="apple-touch-icon" href="/assets/img/apple-touch-icon.png" />
+        <link rel="icon" type="image/svg+xml" href="/assets/img/icons/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icons/favicon-16x16.png" />
+        <link rel="apple-touch-icon" href="/assets/img/icons/apple-touch-icon.png" />
       </Head>
       <Component {...pageProps} />
     </ParallaxProvider>
