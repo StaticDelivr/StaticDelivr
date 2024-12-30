2666d0584d1b9c8ac4a090dc2c90c524d744b40f
Coozywana <73876112+coozywana@users.noreply.github.com>
2024-12-31 01:52:54 +1100

    Add dynamic canonical URL support in _app.js

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,15 @@
 import '../styles/globals.css'; // Import your Tailwind CSS file
 import { ParallaxProvider } from 'react-scroll-parallax';
 import Head from 'next/head'; // Import Head for managing the <head> section
+import { useRouter } from 'next/router'; // Import useRouter to access the current route
 import { StaticDelivr } from 'staticdelivr';
 
 StaticDelivr.set({ baseURL: 'https://staticdelivr.com' });
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter(); // Get the current route
+  const baseUrl = 'https://staticdelivr.com';
+
   return (
     <ParallaxProvider>
       <Head>
@@ -14,6 +18,8 @@ function MyApp({ Component, pageProps }) {
         <link rel="icon" type="image/png" sizes="32x32" href="/assets/img/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/assets/img/icons/favicon-16x16.png" />
         <link rel="apple-touch-icon" href="/assets/img/icons/apple-touch-icon.png" />
+        {/* Add the canonical URL dynamically */}
+        <link rel="canonical" href={`${baseUrl}${router.asPath}`} />
       </Head>
       <Component {...pageProps} />
     </ParallaxProvider>
